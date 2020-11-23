4ee495fec0837ad41193bc1e9a083e425dd0ecf8
Duong Tran Tu Minh <duongtrantuminh14@gmail.com>
2020-11-23 17:03:42 +0700

    HELLO

--- app.py
+++ app.py
@@ -3,13 +3,16 @@ from server import create_app
 
 app = create_app()
 
+
 @app.route('/')
 def hello_world():
     return render_template("index.html")
 
+
 @app.route('/admin')
 def admin():
     return render_template("Admin/index.html")
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
--- server/constant.py
+++ server/constant.py
@@ -1,13 +1,13 @@
 from enum import Enum
 
+
 class Constant:
     ROLE = {
         'USER_ROLE': Enum(
-        value='USER_ROLE',
-        names=[
-            ('user', 'user'),
-            ('admin', 'admin')
-        ]
-    )
+            value='USER_ROLE',
+            names=[
+                ('user', 'user'),
+                ('admin', 'admin')
+            ]
+        )
     }
-
